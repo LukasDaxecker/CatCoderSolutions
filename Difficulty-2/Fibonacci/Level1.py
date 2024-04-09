9293b043307baed242c108b7fb956a04cc915f77
LukasDaxecker <daxecker.lukas@gmail.com>
2024-04-09 20:11:48 +0200

    Fibonacci

---
+++ Difficulty-2/Fibonacci/Level1.py
@@ -0,0 +1,13 @@
+n = 6
+
+if n >= 2:
+        add = sum = 1
+        
+        for i in range(n-2):
+                temp = sum
+                sum += add
+                add = temp
+else:
+        sum = n
+
+print(sum)
